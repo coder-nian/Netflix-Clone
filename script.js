f85db2b8f229d3d5afd6ae76fda3651463cdff52
coder-nian <nomahmad10@gmail.com>
2023-08-12 15:11:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+  getOriginals()
+  getTrendingNow()
+  getTopRated()
+}
+
+function fetchMovies(url, dom_element, path_type) {
+  fetch(url)
+    .then(response => {
+      if(response.ok)
+      {
+        return response.json()
+      }
+      else
+      {
+        throw new Error('Something went wrong!')
+      }
+    })
+    .then(data => {
+      showMovies(data, dom_element, path_type)
+    })
+    .catch(error_data => {
+      console.log(error_data)
+    })
+}
+
+showMovies = (movies, dom_element, path_type) => {
+  const moviesEl = document.querySelector(dom_element)
+
+  for (var movie of movies.results)
+  {
+    const imgElement = document.createElement('img')
+    imgElement.setAttribute('data-id', movie.id)
+    imgElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
+    moviesEl.appendChild(imgElement)
+  }
+}
+
+function getOriginals() {
+  const url = 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
+  fetchMovies(url, '.original__movies', 'poster_path')
+}
+
+function getTrendingNow() {
+  const url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
+  fetchMovies(url, '#trending', 'backdrop_path')
+}
+
+function getTopRated() {
+  const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
+  fetchMovies(url, '#top_rated', 'backdrop_path')
+}
